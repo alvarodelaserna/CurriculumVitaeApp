381b66ab2db3a8cc33716625df9087dddae6a9ed
alvarodelaserna <alvaro.delaserna@gmail.com>
2015-09-25 11:35:10 +0200

    Create 'CustomGestureDetector.java', add 'back' functionality when user swipes right

--- app/src/main/java/com/asm/curriculumvitaeapp/activities/MainActivity.java
+++ app/src/main/java/com/asm/curriculumvitaeapp/activities/MainActivity.java
@@ -1,16 +1,18 @@
 package com.asm.curriculumvitaeapp.activities;
 
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.widget.ImageButton;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import com.asm.curriculumvitaeapp.R;
 import com.asm.curriculumvitaeapp.core.CurriculumVitaeAppUtils;
+import com.asm.curriculumvitaeapp.core.CustomGestureDetector;
 import com.asm.curriculumvitaeapp.fragments.EducationFragment;
 import com.asm.curriculumvitaeapp.fragments.ExperienceFragment;
 import com.asm.curriculumvitaeapp.fragments.JobFragment;
@@ -26,8 +28,17 @@ public class MainActivity extends FragmentActivity implements MainFragment.OnFra
 
     private final String TAG = MainActivity.class.getCanonicalName();
     private Context mContext;
-    private static final String TAG_MAIN = "main";
+    private static final String TAG_MAIN = "main", TAG_EXPERIENCE = "experience", TAG_SKILLS = "skills",
+            TAG_PERSONAL_INFORMATION = "personal", TAG_EDUCATION = "education", TAG_TAPAP = "tapap",
+            TAG_PROGRESSIVE_BLUE = "progressive", TAG_TECHNOSITE = "technosite";
     private MainFragment mainFragment;
+    private EducationFragment educationFragment;
+    private ExperienceFragment experienceFragment;
+    private JobFragment tapapFragment, progressiveBlueFragment, technositeFragment;
+    private PersonalInformationFragment personalInformationFragment;
+    private SkillsFragment skillsFragment;
+
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +59,21 @@ public class MainActivity extends FragmentActivity implements MainFragment.OnFra
             fragmentTransaction.add(R.id.fragment_container, mainFragment, TAG_MAIN);
             fragmentTransaction.commit();
         }
+
+        CustomGestureDetector customGestureDetector = new CustomGestureDetector(this);
+        gestureDetector = new GestureDetector(this, customGestureDetector);
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+
+        return super.onTouchEvent(event);
+    }
+
 }
