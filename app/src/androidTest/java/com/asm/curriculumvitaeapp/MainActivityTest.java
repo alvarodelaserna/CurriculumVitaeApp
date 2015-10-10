554540d6b335537c8b25ac52da884a13f6ef1f1e
alvarodelaserna <alvaro.delaserna@gmail.com>
2015-10-10 12:34:38 +0200

    Refactor MainActivityTest

--- app/src/androidTest/java/com/asm/curriculumvitaeapp/MainActivityTest.java
+++ app/src/androidTest/java/com/asm/curriculumvitaeapp/MainActivityTest.java
@@ -1,6 +1,5 @@
 package com.asm.curriculumvitaeapp;
 
-import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -8,19 +7,17 @@ import android.widget.TextView;
 
 import com.asm.curriculumvitaeapp.activities.MainActivity;
 
-import org.junit.runner.RunWith;
-
 /**
  * Created by alvaro on 21/09/15.
  */
-@RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private MainActivity mainActivity;
-    private TextView my_name, button_show_personal_info, button_show_professional_experience, button_show_education;
+    private TextView my_name;
+    private ImageButton button_show_personal_info, button_show_professional_experience, button_show_education, button_show_skills;
     private ImageView my_picture;
     private ImageButton ic_action_call, ic_action_email;
-    private String MY_NAME, EDUCATION, EXPERIENCE, PERSONAL_INFO;
+    private String MY_NAME;
 
     public MainActivityTest(Class<MainActivity> activityClass) {
         super(activityClass);
@@ -31,16 +28,14 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         super.setUp();
         mainActivity = getActivity();
         MY_NAME = mainActivity.getResources().getString(R.string.my_name);
-        EDUCATION = mainActivity.getResources().getString(R.string.education);
-        EXPERIENCE = mainActivity.getResources().getString(R.string.experience);
-        PERSONAL_INFO = mainActivity.getResources().getString(R.string.personal_info);
         my_name = (TextView) mainActivity.findViewById(R.id.name);
         my_picture = (ImageView) mainActivity.findViewById(R.id.picture_of_me);
         ic_action_call = (ImageButton) mainActivity.findViewById(R.id.ic_action_call);
         ic_action_email = (ImageButton) mainActivity.findViewById(R.id.ic_action_email);
-        button_show_personal_info = (TextView) mainActivity.findViewById(R.id.button_show_personal_info);
-        button_show_professional_experience = (TextView) mainActivity.findViewById(R.id.button_show_professional_experience);
-        button_show_education = (TextView) mainActivity.findViewById(R.id.button_show_education);
+        button_show_personal_info = (ImageButton) mainActivity.findViewById(R.id.button_show_personal_info);
+        button_show_professional_experience = (ImageButton) mainActivity.findViewById(R.id.button_show_professional_experience);
+        button_show_education = (ImageButton) mainActivity.findViewById(R.id.button_show_education);
+        button_show_skills = (ImageButton) mainActivity.findViewById(R.id.button_show_skills);
 
     }
 
@@ -57,8 +52,5 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     public void testText() {
         assertEquals(MY_NAME, (String) my_name.getText());
-        assertEquals(EDUCATION, (String) button_show_education.getText());
-        assertEquals(PERSONAL_INFO, (String) button_show_personal_info.getText());
-        assertEquals(EXPERIENCE, (String) button_show_professional_experience.getText());
     }
 }
